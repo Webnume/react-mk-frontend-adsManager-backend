b385c6c312062cdf516fac860808d88669b6f4a5
Yves G <contact@webnumerik.fr>
2022-07-19 13:38:36 +0200

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+//for Heroku deployment
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3002
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
